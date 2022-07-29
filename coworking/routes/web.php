c2e42a103bb99c9e2fb1e6d733ff5d20ca07f59d
Projects-Lab <54554407+Projects-Lab@users.noreply.github.com>
2022-07-29 09:42:03 -0500

    Implementacion de Landing con el login

--- coworking/routes/web.php
+++ coworking/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::middleware([
     'auth:sanctum',
@@ -26,3 +26,8 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::view('/','landingpage')->name('home');
+ Route::view('/about','about')->name('about');
+Route::view('/contact','contact')->name('contact');
+Route::view('/service','service')->name('service');
